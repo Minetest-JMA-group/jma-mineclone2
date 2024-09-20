3c30a3d73c89735ff49c786a0e510e493337d906
fancyfinn9 <fancyfinn9@gmail.com>
2024-09-20 10:39:45 +0100

    mcl_fish_traps: Nerf catch rate
    Decrease catch speed: 30secs -> 60secs
    Decrease catch chance: 100% -> 50%
    Further changes, such as buffs, may be made in the future, this nerf was to make fish traps less OP.

--- mods/other/mcl_fish_traps/init.lua
+++ mods/other/mcl_fish_traps/init.lua
@@ -195,8 +195,8 @@ minetest.register_abm({
 minetest.register_abm({
 	label = "Run fish trap",
 	nodenames = {"mcl_fish_traps:fishing_trap_water"},
-	interval = 30,
-	chance = 1,
+	interval = 60,
+	chance = 2,
 	action = function(pos,value)
 		local meta = minetest.get_meta(pos)
 		local inv = meta:get_inventory()
