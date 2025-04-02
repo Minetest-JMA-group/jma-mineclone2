0290af97422fa650bd28c3720aab9af9db8b8c28
fancyfinn9 <fancyfinn9@gmail.com>
2025-04-02 18:38:49 +0100

    mcl_fish_traps: Fix worm itemstring in dirt drops

--- mods/ITEMS/mcl_core/nodes_base.lua
+++ mods/ITEMS/mcl_core/nodes_base.lua
@@ -513,9 +513,32 @@ minetest.register_node("mcl_core:dirt", {
 	drop = {
 		max_items = 1,
 		items = {
-			{items = {"mcl_fish_trap:worm", "mcl_core:dirt"},rarity = 20},
+			{items = {"mcl_fish_traps:earth_worm", "mcl_core:dirt"},rarity = 20},
 			{items = {"mcl_core:dirt"}}
 		}
+	},
+	_mcl_fortune_drop = {
+		[1] = {
+			max_items = 1,
+			items = {
+				{items = {"mcl_fish_traps:earth_worm", "mcl_core:dirt"},rarity = 17},
+				{items = {"mcl_core:dirt"}}
+			}
+		},
+		[2] = {
+			max_items = 1,
+			items = {
+				{items = {"mcl_fish_traps:earth_worm", "mcl_core:dirt"},rarity = 15},
+				{items = {"mcl_core:dirt"}}
+			}
+		},
+		[3] = {
+			max_items = 1,
+			items = {
+				{items = {"mcl_fish_traps:earth_worm", "mcl_core:dirt"},rarity = 10},
+				{items = {"mcl_core:dirt"}}
+			}
+		},
 	},
 	groups = {handy=1,shovely=1, dirt=1,soil=1, soil_sapling=2, soil_sugarcane=1, cultivatable=2, enderman_takable=1, building_block=1, path_creation_possible=1},
 	sounds = mcl_sounds.node_sound_dirt_defaults(),
