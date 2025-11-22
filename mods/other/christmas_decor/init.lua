8a3fa22f6ac7993bbb0eb084321a7a3bf4b88d8e
fancyfinn9 <fancyfinn9@proton.me>
2025-11-22 11:24:15 +0000

    christmas_decor: Fix check for mcl_core and mineclonefood

--- mods/other/christmas_decor/init.lua
+++ mods/other/christmas_decor/init.lua
@@ -14,8 +14,8 @@ local depends = {
 	vessels = minetest.get_modpath("vessels"),
 	wool = minetest.get_modpath("wool"),
 	xpanes = minetest.global_exists("xpanes"),
-	mcl_core = minetest.global_exists("mcl_core"),
-	mineclonefood = minetest.global_exists("mineclonefood"),
+	mcl_core = minetest.get_modpath("mcl_core"),
+	mineclonefood = minetest.get_modpath("mineclonefood"),
 }
 
 local function default_sounds(name)
